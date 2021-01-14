12094e569c895518e1ddf84ff67d9c1831408e15
RanimAmor <72764236+RanimAmor@users.noreply.github.com>
2021-01-14 12:21:14 +0100

    auth routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {​​AuthModule}​​ from './auth/auth.module';
+import {​​ RouterModule }​​ from "@angular/router";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
